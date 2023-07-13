deed1369f713e0730a3f2e8bdd87875cc88b576c
vikram390 <112178932+vikram390@users.noreply.github.com>
2023-07-13 14:19:34 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,11 +7,18 @@ cd /var/www/html
 npm install
 
 # Build the Angular application
-ng build --configuration production
+npm run build
 
-# Set file permissions
+# Set ownership and permissions for Nginx
 chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+# Configure Nginx to serve the Angular application
+rm /etc/nginx/sites-enabled/default
+cp nginx.conf /etc/nginx/sites-available/angular-app
+ln -s /etc/nginx/sites-available/angular-app /etc/nginx/sites-enabled/
 
 # Restart Nginx service
 service nginx restart
+
 
