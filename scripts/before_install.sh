3a0daaf21e3b75214f7bba32dfacfe954fb1a23d
vikram390 <112178932+vikram390@users.noreply.github.com>
2023-07-15 11:42:34 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
-# Update the system packages
-apt update
-apt upgrade -y
+# Remove existing application files
+sudo rm -rf /var/www/html/*
 
-# Install Node.js
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y nodejs
+# Remove Nginx default configuration
+sudo rm /etc/nginx/sites-enabled/default
 
-# Install Nginx
-apt-get install -y nginx
-
-# Stop Nginx service if it's running
-service nginx stop
-
-# Remove the default Nginx configuration file
-rm /etc/nginx/sites-enabled/default
-
-# Create a new Nginx configuration file for your Angular application
-cat >/etc/nginx/sites-available/angular-app <<EOL
+# Create a new Nginx configuration file
+sudo tee /etc/nginx/conf.d/angular-app.conf > /dev/null <<EOF
 server {
-        listen 80;
-        listen [::]:80;
+    listen 80;
+    root /var/www/html;
+    index index.html index.htm;
+    server_name 54.82.54.221;
 
-        server_name 54.82.54.221;
-
-        root /var/www/html;
-        index index.html;
-
-        location / {
-                try_files $uri $uri/ /index.html;
+    location / {
+        try_files \$uri \$uri/ /index.html;
     }
 }
-EOL
+EOF
 
-# Enable the Nginx configuration by creating a symbolic link
-ln -s /etc/nginx/sites-available/angular-app /etc/nginx/sites-enabled/
+# Test Nginx configuration
+sudo nginx -t
 
-# Restart Nginx service
-service nginx restart
+# Start Nginx service
+sudo service nginx start
+
+
 
 
