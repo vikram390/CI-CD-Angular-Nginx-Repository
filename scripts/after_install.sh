80a0cf3721272e5aabfb297f1403f668ed83e7fc
vikram390 <112178932+vikram390@users.noreply.github.com>
2023-07-15 12:08:52 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+apt update
+apt upgrade
 
 # Install Node.js and npm
-curl -sL https://deb.nodesource.com/setup_16.13 | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install Angular CLI globally
-sudo npm install -g @angular/cli
+sudo npm install -g @angular/cli@latest
 
 # Install project dependencies
 cd /var/www/html
